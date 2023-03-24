822d43f9e1760e96a3e44a06907fbff58c36df8e
Sindre Vatnaland <69798990+SindreVatnaland@users.noreply.github.com>
2023-03-24 05:56:29 +0100

    Init leaderboard page

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { eventEmitter } from '$lib/utils/store.svelte';
 	import { fade } from 'svelte/transition';
+
+	// TODO add filter dropdowns
+	// Add and generate 500 lines of dummy data
 </script>
 
 <main
@@ -9,7 +12,16 @@
 	in:fade={{ delay: 50, duration: 150 }}
 	out:fade={{ duration: 300 }}
 >
-	<h6>Leaderboard</h6>
+	<div class="w-full h-full place-items-start grid grid-cols-12 grid-rows-6 gap-2 px-2 md:px-20">
+		<div class="w-full grid grid-cols-12 col-span-12 row-span-1 h-full gap-x-2">
+			<div class="h-full border col-span-3" />
+			<div class="h-full border col-span-3" />
+			<div class="h-full border col-span-3" />
+		</div>
+		<div class="w-full h-full row-span-5 col-span-12 border">
+			<div class="w-full h-12 border">Player</div>
+		</div>
+	</div>
 </main>
 
 <style>
