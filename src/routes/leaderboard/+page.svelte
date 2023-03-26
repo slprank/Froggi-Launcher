809f4bbb3a94143d08f3906dfe733a803051e1a1
Sindre Vatnaland <69798990+SindreVatnaland@users.noreply.github.com>
2023-03-26 21:59:17 +0200

    Leaderboard list format

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import TextFitMulti from '$lib/components/TextFitMulti.svelte';
 	import { eventEmitter } from '$lib/utils/store.svelte';
 	import { fade } from 'svelte/transition';
 
@@ -12,14 +13,77 @@
 	in:fade={{ delay: 50, duration: 150 }}
 	out:fade={{ duration: 300 }}
 >
-	<div class="w-full h-full place-items-start grid grid-cols-12 grid-rows-6 gap-2 px-2 md:px-20">
-		<div class="w-full grid grid-cols-12 col-span-12 row-span-1 h-full gap-x-2">
-			<div class="h-full border col-span-3" />
-			<div class="h-full border col-span-3" />
-			<div class="h-full border col-span-3" />
+	<div class="w-full h-12 place-items-start gap-2 px-2 md:px-20 grid content-center">
+		<h1 class="text-2xl m-0">Leaderboard</h1>
+	</div>
+	<div class="w-full h-16 place-items-start flex gap-2 px-2 md:px-20">
+		<div class="w-16 h-full border">Region</div>
+		<div class="w-16 h-full border">Character</div>
+		<div class="w-16 h-full border">Something</div>
+	</div>
+
+	<div class="w-full h-12 grid grid-cols-12 col-span-12 px-2 md:px-20">
+		<div
+			class="w-full h-full grid grid-cols-12 col-span-12 border-b-1 border-gray-500 rounded-t-md bg-black bg-opacity-50"
+		>
+			<div class="w-full h-12 col-span-2 grid content-center justify-center">
+				<h1 class="text-white text-xl">RANK</h1>
+			</div>
+			<div class="w-full h-12 col-span-3 grid content-center justify-start">
+				<h1 class="text-white text-xl">PLAYER</h1>
+			</div>
+			<div class="w-full h-12 col-span-3 grid content-center justify-start">
+				<h1 class="text-white text-xl">CHARACTERS</h1>
+			</div>
+			<div class="w-full h-12 col-span-2 grid content-center justify-center">
+				<h1 class="text-white text-xl">RATING</h1>
+			</div>
+			<div class="w-full h-12 col-span-2 grid content-center justify-center">
+				<h1 class="text-white text-xl">W/L</h1>
+			</div>
 		</div>
-		<div class="w-full h-full row-span-5 col-span-12 border">
-			<div class="w-full h-12 border">Player</div>
+	</div>
+
+	<div class="w-full place-items-start grid grid-cols-12 px-2 md:px-20 overflow-auto">
+		<div
+			class="w-full h-12 grid grid-cols-12 col-span-12 border-b-1 border-gray-500 bg-black bg-opacity-25"
+		>
+			<div class="w-full h-12 col-span-2 grid content-center justify-center">
+				<h1 class="text-white text-xl">1</h1>
+			</div>
+			<div class="w-full h-12 col-span-3 grid grid-rows-2 content-center">
+				<h1 class="w-full h-full max-w-full text-white text-xl">GATE OF STEINER</h1>
+				<h1 class="w-full h-full max-w-full text-gray-500 text-sm">ABBE#0</h1>
+			</div>
+			<div class="w-full h-12 col-span-3 grid content-center justify-start">
+				<h1 class="text-white text-xl">ICO</h1>
+			</div>
+			<div class="w-full h-12 col-span-2 grid content-center justify-center">
+				<h1 class="text-white text-xl">2891.3</h1>
+			</div>
+			<div class="w-full h-12 col-span-2 grid content-center justify-center">
+				<h1 class="text-white text-xl">1024/194</h1>
+			</div>
+		</div>
+		<div
+			class="w-full h-12 grid grid-cols-12 col-span-12 border-b-1 border-gray-500 bg-black bg-opacity-50"
+		>
+			<div class="w-full h-12 col-span-2 grid content-center justify-center">
+				<h1 class="text-white text-xl">2</h1>
+			</div>
+			<div class="w-full h-12 col-span-3 grid grid-rows-2 content-center">
+				<h1 class="w-full h-full max-w-full text-white text-xl">Nicki</h1>
+				<h1 class="w-full h-full max-w-full text-gray-500 text-sm">NICKI#1</h1>
+			</div>
+			<div class="w-full h-12 col-span-3 grid content-center justify-start">
+				<h1 class="text-white text-xl">ICO</h1>
+			</div>
+			<div class="w-full h-12 col-span-2 grid content-center justify-center">
+				<h1 class="text-white text-xl">2791.3</h1>
+			</div>
+			<div class="w-full h-12 col-span-2 grid content-center justify-center">
+				<h1 class="text-white text-xl">1024/1</h1>
+			</div>
 		</div>
 	</div>
 </main>
