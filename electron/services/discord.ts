912e9d95e326e5274d59efcee61d907880a322ba
Sindre Vatnaland <sindrevatnaland97@gmail.com>
2023-08-23 22:46:28 +0200

    Discord support

--- electron/services/discord.ts
+++ electron/services/discord.ts
@@ -1,18 +1,39 @@
 import { ElectronLog } from 'electron-log';
 import { inject, singleton } from 'tsyringe';
 import EventEmitter from 'events';
+import { Client, Presence } from "discord-rpc"
 
 @singleton()
 export class Discord {
+	rpc: Client;
+	activity: Presence = {
+		details: "Menu",
+		state: "Ranked",
+		startTimestamp: undefined,
+		endTimestamp: undefined,
+		smallImageKey: "character_2_0",
+		smallImageText: "Fox",
+		largeImageKey: "stage_32",
+		largeImageText: "Final Destination",
+		buttons: []
+	};
 	constructor(
 		@inject("ElectronLog") public log: ElectronLog,
 		@inject("EventEmitter") public eventEmitter: EventEmitter,
 	) {
+		this.rpc = new Client({ transport: "ipc" })
+		this.rpc.login({ clientId: "1143955754643112016" }).catch(err => this.log.error("err", err))
 		this.initDiscordJs()
 	}
 
 	initDiscordJs() {
-		this.log.info("Initializing memory read")
+		this.rpc.on("ready", () => {
+			console.log("rpc ready", this.activity)
+			setInterval(() => {
+				this.rpc.setActivity(this.activity).catch(err => this.log.error(err))
+			}, 1500)
+		})
+		this.log.info("Initializing discord")
 		this.initDiscordEvents()
 	}
 
