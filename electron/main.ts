fc3774cd019024dadec3f8305e36007a08320fe9
Sindre Vatnaland <69798990+SindreVatnaland@users.noreply.github.com>
2024-01-08 21:18:34 +0100

    Tray icon fix

--- electron/main.ts
+++ electron/main.ts
@@ -88,8 +88,10 @@ function createWindow(): BrowserWindow {
 }
 
 function createTray(): Tray {
-	const image = nativeImage.createFromPath(path.join(__dirname, '../frontend/static/icon.png'));
+	const imagePath = path.join(__dirname, '../../build/icon.png');
+	const image = nativeImage.createFromPath(imagePath);
 	tray = new Tray(image.resize({ width: 16, height: 16 }));
+	app.dock.setIcon(image);
 	tray.setToolTip('Froggi');
 
 	const contextMenu = Menu.buildFromTemplate([
