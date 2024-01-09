98d910808a8303d02bf8d267f062f0612b8b241f
Sindre Vatnaland <69798990+SindreVatnaland@users.noreply.github.com>
2024-01-09 14:56:19 +0100

    Post game player 1/2 win condition

--- frontend/src/lib/components/obs/overlays/element/animations/visibilityConditions/PostGameVisibilityOptions.svelte
+++ frontend/src/lib/components/obs/overlays/element/animations/visibilityConditions/PostGameVisibilityOptions.svelte
@@ -20,101 +20,109 @@
 			if (winnerIndex === player?.playerIndex) return true;
 		if (option[VisibilityOption.PostGameCurrentPlayerWin] === VisibilityToggle.False)
 			if (winnerIndex !== player?.playerIndex) return true;
+		if (option[VisibilityOption.PostGamePlayer1Win] === VisibilityToggle.True)
+			if (winnerIndex === players.at(0)?.playerIndex) return true;
+		if (option[VisibilityOption.PostGamePlayer1Win] === VisibilityToggle.False)
+			if (winnerIndex !== players.at(0)?.playerIndex) return true;
+		if (option[VisibilityOption.PostGamePlayer2Win] === VisibilityToggle.True)
+			if (winnerIndex === players.at(1)?.playerIndex) return true;
+		if (option[VisibilityOption.PostGamePlayer2Win] === VisibilityToggle.False)
+			if (winnerIndex !== players.at(1)?.playerIndex) return true;
 
 		if (option[VisibilityOption.PostGamePlayer1Stock1] === VisibilityToggle.True)
 			if (
-				(gameStats.lastFrame.players[players?.at(0)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(0)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) >= 1
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer1Stock1] === VisibilityToggle.False)
 			if (
-				(gameStats.lastFrame.players[players?.at(0)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(0)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) < 1
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer1Stock2] === VisibilityToggle.True)
 			if (
-				(gameStats.lastFrame.players[players?.at(0)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(0)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) >= 2
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer1Stock2] === VisibilityToggle.False)
 			if (
-				(gameStats.lastFrame.players[players?.at(0)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(0)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) < 2
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer1Stock3] === VisibilityToggle.True)
 			if (
-				(gameStats.lastFrame.players[players?.at(0)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(0)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) >= 3
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer1Stock3] === VisibilityToggle.False)
 			if (
-				(gameStats.lastFrame.players[players?.at(0)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(0)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) < 3
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer1Stock4] === VisibilityToggle.True)
 			if (
-				(gameStats.lastFrame.players[players?.at(0)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(0)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) >= 4
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer1Stock4] === VisibilityToggle.False)
 			if (
-				(gameStats.lastFrame.players[players?.at(0)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(0)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) < 4
 			)
 				return true;
 
 		if (option[VisibilityOption.PostGamePlayer2Stock1] === VisibilityToggle.True)
 			if (
-				(gameStats.lastFrame.players[players?.at(1)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(1)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) >= 1
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer2Stock1] === VisibilityToggle.False)
 			if (
-				(gameStats.lastFrame.players[players?.at(1)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(1)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) < 1
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer2Stock2] === VisibilityToggle.True)
 			if (
-				(gameStats.lastFrame.players[players?.at(1)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(1)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) >= 2
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer2Stock2] === VisibilityToggle.False)
 			if (
-				(gameStats.lastFrame.players[players?.at(1)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(1)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) < 2
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer2Stock3] === VisibilityToggle.True)
 			if (
-				(gameStats.lastFrame.players[players?.at(1)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(1)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) >= 3
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer2Stock3] === VisibilityToggle.False)
 			if (
-				(gameStats.lastFrame.players[players?.at(1)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(1)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) < 3
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer2Stock4] === VisibilityToggle.True)
 			if (
-				(gameStats.lastFrame.players[players?.at(1)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(1)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) >= 4
 			)
 				return true;
 		if (option[VisibilityOption.PostGamePlayer2Stock4] === VisibilityToggle.False)
 			if (
-				(gameStats.lastFrame.players[players?.at(1)?.playerIndex ?? -1]?.post
+				(gameStats.lastFrame?.players[players?.at(1)?.playerIndex ?? -1]?.post
 					.stocksRemaining ?? 0) < 4
 			)
 				return true;
