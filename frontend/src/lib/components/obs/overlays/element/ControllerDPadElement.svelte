a11d8b85428bcaca9dba4d8a7f4418a21b0b741d
Sindre Vatnaland <69798990+SindreVatnaland@users.noreply.github.com>
2024-01-11 12:42:24 +0100

    Dpad fix

--- frontend/src/lib/components/obs/overlays/element/ControllerDPadElement.svelte
+++ frontend/src/lib/components/obs/overlays/element/ControllerDPadElement.svelte
@@ -7,38 +7,43 @@
 	export let buttonPresses: ControllerButtons | undefined;
 </script>
 
-<div class="w-full h-full flex justify-center object-contain relative">
+<div class="w-full h-full flex justify-center items-center relative">
 	<img
-		style={`${style.cssValue}; ${dataItem?.data.advancedStyling}; ${style.shadow}`}
+		class="w-full h-full absolute object-contain"
+		style={` ${style.cssValue}; ${dataItem?.data.advancedStyling}; ${style.shadow};`}
 		src={`/image/controller-buttons/DPadGate.png`}
-		alt="button"
+		alt="DPadGate"
 	/>
 	{#if buttonPresses?.isDPadUpPressed}
 		<img
-			style={`${style.cssValue}; ${dataItem?.data.advancedStyling}; ${style.shadow}`}
+			class="w-full h-full absolute object-contain"
+			style={`absolute ${style.cssValue}; ${dataItem?.data.advancedStyling}; ${style.shadow}`}
 			src={`/image/controller-buttons/DPadUp.png`}
-			alt="button"
+			alt="DPadUp"
 		/>
 	{/if}
 	{#if buttonPresses?.isDPadDownPressed}
 		<img
-			style={`${style.cssValue}; ${dataItem?.data.advancedStyling}; ${style.shadow}`}
+			class="w-full h-full absolute object-contain"
+			style={`absolute ${style.cssValue}; ${dataItem?.data.advancedStyling}; ${style.shadow}`}
 			src={`/image/controller-buttons/DPadDown.png`}
-			alt="button"
+			alt="DPadDown"
 		/>
 	{/if}
 	{#if buttonPresses?.isDPadLeftPressed}
 		<img
-			style={`${style.cssValue}; ${dataItem?.data.advancedStyling}; ${style.shadow}`}
+			class="w-full h-full absolute object-contain"
+			style={`absolute ${style.cssValue}; ${dataItem?.data.advancedStyling}; ${style.shadow}`}
 			src={`/image/controller-buttons/DPadLeft.png`}
-			alt="button"
+			alt="DPadLeft"
 		/>
 	{/if}
 	{#if buttonPresses?.isDPadRightPressed}
 		<img
-			style={`${style.cssValue}; ${dataItem?.data.advancedStyling}; ${style.shadow}`}
+			class="w-full h-full absolute object-contain"
+			style={`absolute ${style.cssValue}; ${dataItem?.data.advancedStyling}; ${style.shadow}`}
 			src={`/image/controller-buttons/DPadRight.png`}
-			alt="button"
+			alt="DPadRight"
 		/>
 	{/if}
 </div>
